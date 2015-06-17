8fce1bcc9c70c795fc62a670ac0165c3c742d501
abeytom07 <abey.tom@appdynamics.com>
2015-06-17 14:20:47 -0400

    Varnish

---
+++ varnish/build.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+docker build -t extensions/varnish_with_vagent2 .
---
+++ varnish/run.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+echo Reading the source files
+source /etc/varnish/varnish.params
+echo Binding to $VARNISH_LISTEN_ADDRESS:$VARNISH_LISTEN_PORT
+/usr/sbin/varnishd \
+    -P /var/run/varnish.pid \
+    -f $VARNISH_VCL_CONF \
+    -a $VARNISH_LISTEN_ADDRESS:$VARNISH_LISTEN_PORT \
+    -T $VARNISH_ADMIN_LISTEN_ADDRESS:$VARNISH_ADMIN_LISTEN_PORT \
+    -t $VARNISH_TTL \
+    -u $VARNISH_USER \
+    -g $VARNISH_GROUP \
+    -S $VARNISH_SECRET_FILE \
+    -s $VARNISH_STORAGE \
+    $DAEMON_OPTS &
+
+varnish-agent -K /etc/varnish/agent_secret start -d
+
