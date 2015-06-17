cbeafbc3c7165618c2744f5c14eac7ce7d4b2c48
abeytom07 <abey.tom@appdynamics.com>
2015-06-16 21:11:33 -0400

    OpenLDAP Docker Image

---
+++ openldap/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f /data/lib/ldap/DB_CONFIG ]; then
+
+    cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
+    chown ldap. /var/lib/ldap/DB_CONFIG
+
+    service slapd start
+    sleep 3
+
+    ldapmodify -Y EXTERNAL -H ldapi:/// -f /root/manager.ldif
+    ldapmodify -Y EXTERNAL -H ldapi:/// -f /root/domain.ldif
+
+    ldapadd -x -D cn=admin,ou=admins,dc=appdynamics,dc=com -w welcome -f /root/base.ldif
+
+    service slapd stop
+    sleep 3
+
+    mkdir /data/lib /data/etc
+    cp -ar /var/lib/ldap /data/lib
+    cp -ar /etc/openldap /data/etc
+fi
+
+rm -rf /var/lib/ldap && ln -s /data/lib/ldap /var/lib/ldap
+rm -rf /etc/openldap && ln -s /data/etc/openldap /etc/openldap
+
+exec /usr/sbin/slapd -h "ldap:/// ldaps:/// ldapi:///" -u ldap -d $DEBUG_LEVEL
